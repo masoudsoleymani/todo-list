2140baeaeca72cbeadcfe345042bcd49039193bb
Masoud <msoolymani@gmail.com>
2020-10-19 18:24:57 +0100

    Updates

---
+++ precache-manifest.d70093e880b87507afd95ccef01cf3ba.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a0c4c1c516488a42d9f5d906c185c8c2",
+    "url": "/todoList-react/index.html"
+  },
+  {
+    "revision": "036365861734f09500c3",
+    "url": "/todoList-react/static/css/main.42656019.chunk.css"
+  },
+  {
+    "revision": "d2b147e9045903a23e55",
+    "url": "/todoList-react/static/js/2.2edda031.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/todoList-react/static/js/2.2edda031.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "036365861734f09500c3",
+    "url": "/todoList-react/static/js/main.ec8617be.chunk.js"
+  },
+  {
+    "revision": "86b11cb75c511cf27e1b",
+    "url": "/todoList-react/static/js/runtime-main.748c8040.js"
+  },
+  {
+    "revision": "44009639c377e1061866222ef6cfd6db",
+    "url": "/todoList-react/static/media/back.44009639.jpg"
+  }
+]);
