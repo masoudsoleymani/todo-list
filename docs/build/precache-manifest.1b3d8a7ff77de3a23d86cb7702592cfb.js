e8ddb71cf71287fb74b5466a458251209e3ea544
Masoud Soleymani <masoud.soleymani@daimler.com>
2021-02-17 17:25:51 +0000

    publish to github

---
+++ docs/build/precache-manifest.1b3d8a7ff77de3a23d86cb7702592cfb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cedbd217923c2ae445fa332672ce1241",
+    "url": "/todoList-react/index.html"
+  },
+  {
+    "revision": "4a3a2006f643d8627348",
+    "url": "/todoList-react/static/css/main.291774b5.chunk.css"
+  },
+  {
+    "revision": "f137f8f4f794b7748b95",
+    "url": "/todoList-react/static/js/2.13efcf8e.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/todoList-react/static/js/2.13efcf8e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4a3a2006f643d8627348",
+    "url": "/todoList-react/static/js/main.26e8c0ad.chunk.js"
+  },
+  {
+    "revision": "86b11cb75c511cf27e1b",
+    "url": "/todoList-react/static/js/runtime-main.748c8040.js"
+  },
+  {
+    "revision": "44009639c377e1061866222ef6cfd6db",
+    "url": "/todoList-react/static/media/back.44009639.jpg"
+  }
+]);
